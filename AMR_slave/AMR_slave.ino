ddef3369d506552982bfdcb33f77a7c6507fdae1
cheolho-desk <gghg0000@gmail.com>
2019-05-05 20:29:03 +0900

    19.05.05 20:28 kch

---
+++ AMR_slave/AMR_slave.ino
@@ -0,0 +1,20 @@
+/*
+ * AMR_slave
+ * 
+ * 
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+// todo: 전류 측정하기
+// todo: 측정값 sd카드에 저장하기
+// todo: 저장되어 있는 값 lcd로 출력하기
+// todo: master의 요청이 들어오면 값 전송하기
---
+++ DCU_master/DCU_master.ino
@@ -0,0 +1,15 @@
+void setup() {
+  
+  
+}
+
+void loop() {
+  
+  
+}
+
+// todo: 특정 slave 한테서 데이터 요구
+
+// todo: 블록체인 통신을 위한 서버 연결
+
+// todo: sd카드 내부에 data(slave 들의 전력 사용량) 부분과 blockchain(data 해쉬값, 이전 블록, 다음 블록의 해쉬값) 부분 나누기
