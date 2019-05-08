0f7006ab6b64237f8b54cd385609ccf74c9e6f97
jeongeonhee <ghj-1018@hanmail.net>
2019-05-08 20:35:37 +0900

    19.05.08 20:35 lyh

--- AMR_slave/AMR_slave.ino
+++ AMR_slave/AMR_slave.ino
@@ -4,18 +4,162 @@
  * 
  */
 
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#include <SD.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+const int chipSelect = 4; //SD카드 모듈에서 cs핀 부분
+
+File myFile;
+String fileName = "filetest.txt";
+
+String data = "";
+int PowerData = 0;
+int totalData = data.toInt();
+
+String ReadData = "";
+String cut = "//";
+
+int TD = 0;
+int ND = 0;
+String AccData = "AccData:";
+String NowData = "NowData:";
+
+void setup() {
+  Serial.begin(9600);
+  lcd.begin();
+  lcd.clear();
+  while (!Serial) {
+    ;
+  }
+  InitializeSDcard();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  // todo: 전류 측정하기 (테스트용으로 가변저항값이 저장되게 해놓음)
+  data = String(analogRead(A0));
+  delay(1000);
+
+  // todo: 측정값 sd카드에 저장하기
+  PowerData = data.toInt();
+  totalData += PowerData;
+  
+  myFile = SD.open(fileName, O_READ | O_WRITE | O_CREAT | O_READ);
+  writeData(String(totalData), String(data));
+
+  // todo: 저장되어 있는 값 lcd로 출력하기
+  delay(1000);
+  
+  myFile = SD.open(fileName);
+  ReadData = readData();
+
+  TD = totalSumSplit(ReadData,cut);
+  ND = NowSplit(ReadData,cut);
+
+  lcdView(0,AccData,TD);
+  lcdView(1,NowData,ND);
 
 }
 
 // todo: 각 슬레이브에 아이디 지정해주기
-// todo: 전류 측정하기
-// todo: 측정값 sd카드에 저장하기
-// todo: 저장되어 있는 값 lcd로 출력하기
 // todo: master의 요청이 들어오면 값 전송하기
+
+
+//SD카드 연결체크
+void InitializeSDcard(){
+  Serial.println("Opening SD Card . . .");
+  delay(500);
+  if(SD.begin(chipSelect))
+  {
+    Serial.println("SD Card ready to use");
+  }else{
+    Serial.println("Failed to open SD Card");
+    return;
+  }
+}
+
+//LCD화면출력
+void lcdView(int Cursor, String view, int value){
+  lcd.setCursor(0, Cursor); //커서 위치 지정
+  lcd.print(view); 
+  lcd.print(value); // 전력값 
+}
+
+
+//데이터 저장
+void writeData(String intotalData, String indata) {
+  
+  if (myFile) {
+    myFile.println(intotalData);
+    myFile.println(indata);
+    myFile.close(); 
+
+  } else {
+    Serial.println("error opening test.txt");
+  }
+}
+
+// 저장된 데이터 읽기
+String readData() { 
+  int data = 0;
+  String total = "";
+
+  if (myFile) {
+    while (myFile.available()) {
+      data = myFile.read();
+
+      if (data != 10 && data != 13) {
+        total += (char)data;
+      } else {
+        total += '/';
+      }
+    }
+    myFile.close();
+  } else {
+    Serial.println("error opening datalog.txt");
+  }
+  return total;
+}
+
+//누적데이터 잘라오기
+int totalSumSplit(String sData, String sSeparator) {
+  int nCount = 0;
+  int nGetIndex = 0 ;
+
+  String totalSum = "";
+
+  String sCopy = sData;
+
+    nGetIndex = sCopy.indexOf(sSeparator);
+
+    if (-1 != nGetIndex){
+
+      totalSum = sCopy.substring(0, nGetIndex);    
+    }  
+  return totalSum.toInt();
+}
+
+//현재 데이터 잘라오기
+int NowSplit(String sData, String sSeparator) {
+  int nCount = 0;
+  int nGetIndex = 0 ;
+  int nGetIndex2 = 0 ;
+  
+  String now = "";
+
+  String sCopy = sData;
+  
+    nGetIndex = sCopy.indexOf(sSeparator);
+    nGetIndex2 = sCopy.indexOf(sSeparator,nGetIndex+2);
+  
+    if (-1 != nGetIndex){
+      
+      now = sCopy.substring(nGetIndex+2,nGetIndex2);
+
+    }  
+  return now.toInt();
+}
