de5e7d0fbd39d14e7a01b63a5accd3376e58bbd9
Cheolho kim <gghg0000@gmail.com>
2019-05-20 14:26:59 +0900

    19.05.20	14:26 kch

---
+++ send_to_server/send_to_server.ino
@@ -0,0 +1,73 @@
+/*
+ * HTTP Client POST Request
+ * Copyright (c) 2018, circuits4you.com
+ * All rights reserved.
+ * https://circuits4you.com 
+ * Connects to WiFi HotSpot. */
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+
+/* Set these to your desired credentials. */
+const char *ssid = "zzeororo";  //ENTER YOUR WIFI SETTINGS
+const char *password = "cjfghq1a1f!@";
+
+//Web/Server address to read/write from 
+const char *host = "192.168.1.2";   
+
+//=======================================================================
+//                    Power on setup
+//=======================================================================
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_OFF);            //Prevents reconnection issue (taking too long to connect)
+  delay(1000);
+  WiFi.mode(WIFI_STA);            //This line hides the viewing of ESP as wifi hotspot
+  
+  WiFi.begin(ssid, password);     //Connect to your WiFi router
+  Serial.println("");
+
+  Serial.print("Connecting");
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  //If connection successful show IP address in serial monitor
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+}
+
+//=======================================================================
+//                    Main Program Loop
+//=======================================================================
+void loop() {
+  HTTPClient http;    //Declare object of class HTTPClient
+
+  String data = "test!";
+
+  //Post Data
+  String postData = "data=" + data;
+  
+  http.begin("http://192.168.1.2:3000/arduino");                          //Specify request destination
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");    //Specify content-type header
+
+  int httpCode = http.POST(postData);   //Send the request
+  String payload = http.getString();    //Get the response payload
+
+  Serial.println(httpCode);   //Print HTTP return code
+  Serial.println(payload);    //Print request response payload
+
+  http.end();  //Close connection
+  
+  delay(5000);  //Post Data at every 5 seconds
+}
+//=======================================================================
